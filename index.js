e373f1371815fec615041a478d2d6ea8ef0819ef
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-03 08:45:36 -0400

    commit tree a70a8124e3e049d3a801b1229d77e78b42bb933b

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+class Polygon {
+    constructor(array){
+        this.array = array
+    }
+
+    get countSides(){
+        return this.array.length
+    }
+
+    get perimeter(){
+        return this.array.reduce((total, current) => total + current)
+    }
+}
+
+
+class Triangle extends Polygon {
+    get isValid(){
+        return this.array.length === 3 && this.array[0] + this.array[1] > this.array[2] && this.array[0] + this.array[2] > this.array[1] && this.array[2] + this.array[1] > this.array[0]
+    }
+}
+
+
+class Square extends Polygon {
+    get isValid(){
+        return this.array.length === 4 && this.array[0] === this.array[0] && this.array[1] === this.array[2] && this.array[2] === this.array[3]
+    }
+
+    get area(){
+        return this.array[0] * this.array[1]
+    }
+}
